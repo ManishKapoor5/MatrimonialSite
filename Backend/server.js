4c55f643d44ae4d9178efc7d1825d4a895ea95a5
Manish Kapoor <115330437+ManishKapoor5@users.noreply.github.com>
2025-04-15 08:09:50 +0530

    Update server.js for production

--- Backend/server.js
+++ Backend/server.js
@@ -72,7 +72,7 @@ app.use(fileupload({
 
 // Serve static files in production
 if (process.env.NODE_ENV === "production") {
-  //app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
