855ec724462e1a473fa56cf55ca83c1cd4214834
Manish Kapoor <115330437+ManishKapoor5@users.noreply.github.com>
2025-04-15 13:00:11 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -70,14 +70,6 @@ app.use(fileupload({
 ));
 
 
-// Serve static files in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
 
 // Socket.io
 // io.on('connection', (socket) => {
